e7e3f5bea1539148a71ed8a45d3f9a9fa4df28d4
akshayb407 <akshayb407@gmail.com>
2022-07-31 22:54:04 +0530

    UC4_Repet_Till_Player_Reaches_WinningPosition_100

--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -7,7 +7,7 @@ namespace SnakeAndLadder
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Snake And Ladder Program!");
-            int player = 1;
+            const int player = 1;
             int position = 0;            
             Console.WriteLine($"Snake And Ladder Played With  Player :" + player+ ";   " +"Starting Position Of the Player is:" + position);  ; ;
            
@@ -16,21 +16,29 @@ namespace SnakeAndLadder
             Console.WriteLine("Die Rolled By the Player And The Number is:"+number);
             int option = random.Next(0, 3);
 
-            switch (option)
+            while (position <= 100)
             {
-                case 0:
-                    Console.WriteLine("No play");
-                    break;
-                case 1:
-                    Console.WriteLine("Ladder");
-                    position = position + number;
-                    break;
-                case 2:
-                    Console.WriteLine("Snake");
-                    position = position - number;
-                    break;
+                switch (option)
+                {
+                    case 0:
+                        Console.WriteLine("No play");
+                        break;
+                    case 1:
+                        Console.WriteLine("Ladder");
+                        position = position + number;
+                        break;
+                    case 2:
+                        Console.WriteLine("Snake");
+                        position = position - number;
+                        break;
 
 
+                }
+                
+                if (position < 0)
+                {
+                    position = 0;
+                }
             }
         }
     }
