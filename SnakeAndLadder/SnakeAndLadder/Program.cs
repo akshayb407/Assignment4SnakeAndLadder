1cbaee47628c54c9d2dde653365a98e1e46992bc
akshayb407 <akshayb407@gmail.com>
2022-07-31 22:03:12 +0530

    UC2_Roll_Die_To_Get_The_Number_Between_1_To_6

--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -8,10 +8,12 @@ namespace SnakeAndLadder
         {
             Console.WriteLine("Welcome to the Snake And Ladder Program!");
             int player = 1;
-            int position = 0;
-
-            Console.WriteLine($"Snake And Ladder Played With  Player :" + player+ "; " +"Starting Position Of the Player is:" + position);  ; ;
+            int position = 0;            
+            Console.WriteLine($"Snake And Ladder Played With  Player :" + player+ ";   " +"Starting Position Of the Player is:" + position);  ; ;
            
+            Random random = new Random();
+            int number = random.Next(0, 7);
+            Console.WriteLine("Die Rolled By the Player And The Number is:"+number);
         }
     }
 }
